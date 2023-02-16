7b434aa445c9663ad4d5f6016ca4f88772deb79b
Senzeni <lingedlamini@gmail.com>
2023-02-16 13:26:58 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
- int main (void)
+/**
+ * main - C program that prints a line with puts function
+ * Return: 0
+ */
+ int main(void)
  {
-     puts("Programming is like building a multilingual puzzle\n";
-     return (0)
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
  }	
