484cf6eda8b35436c5e93acc5fab859f201d0ca9
Senzeni <lingedlamini@gmail.com>
2023-02-16 16:02:51 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Return 1 (Succes)
+ * Return: 1 (Succes)
  */
 int main(void)
-{	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout";
+{	
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return(1);
 }
