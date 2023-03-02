ac17f3981ba27d4080d41eea43dd1a2a977acfab
Senzeni <lingedlamini@gmail.com>
2023-03-02 13:51:24 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,9 +17,9 @@ char *rot13(char *s)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == datal[j])
+			if (s[i] == data1[j])
 			{
-				s[i] datarot(j);
+				s[i] =  datarot[j];
 				break;
 			}
 		}
