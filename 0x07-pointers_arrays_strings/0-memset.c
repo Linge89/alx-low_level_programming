22faec4617eaee90a7bac686dd4517eeb5d72f36
Senzeni <lingedlamini@gmail.com>
2023-03-06 12:39:28 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with constant bytes
+ * @s: pointer
+ * @b: constant byte
+ * @n: size of the memory
+ * Return: pointer to the memory s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i=0 ; i < n; i++)
+		s[i] = b;
+	return (s);
+}
