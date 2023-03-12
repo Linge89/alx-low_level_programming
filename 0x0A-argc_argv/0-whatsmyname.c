1c9f217b2333e96d40964ae351172af756ba5284
Senzeni <lingedlamini@gmail.com>
2023-03-12 07:47:41 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of arguments passed
+ * @argv: names of those arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
