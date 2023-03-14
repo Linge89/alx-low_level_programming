ccc73b8077d0a3e224ebe15bea4b525a2b11b29d
Senzeni <lingedlamini@gmail.com>
2023-03-14 12:10:58 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,68 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
 /**
-		
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;	
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}	
-	if (f == 0)	
-		return (0);
-	return (n);
-}
-
-/**
- * main - program that multiplies two numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
-
 int main(int argc, char *argv[])
 {
-	int j;
-	int k;
-
-	if (argc == 3)
+	if (argc != 3)
 	{
-		j = atoi(argv[1]);
-		k = atoi(argv[2]);
-		printf("%d\n", j * k);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-		printf("error\n");
-	return (1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
