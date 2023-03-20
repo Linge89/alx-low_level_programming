27a08e978ed3097d546df81d82ded57c486776e2
Senzeni <lingedlamini@gmail.com>
2023-03-20 05:04:58 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,20 @@
+#ifndef "3-function_like_macro.h"
+#define "3-function_like_macro.h"
+#include <stdio.h>
+#include <stdlib>
 #define ABS(x)
+
+/**
+ * main - computes value of x
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = ABS(X) 10;
+
+	printf("%d\n", i)
+		return (0);
+}
+#endif
