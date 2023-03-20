9a85c5ab2cbdfb203e6c1cd3d247eed3c53acf8b
Senzeni <lingedlamini@gmail.com>
2023-03-20 04:09:37 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#include <stdlib.h>
+#define ABS
 
 /**
  * main - check the code
@@ -12,7 +12,7 @@ int main(void)
 	int i;
 	int j;
 
-	i = ABS(-98) * 10;
+	i = ABS(98) * 10;
 	j = ABS(98) * 10;
 	printf("%d, %d\n", i, j);
 	return (0);
