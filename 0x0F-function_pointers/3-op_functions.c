1708d4d4829c999ca81a0315e1e35a806f5bdaa0
Senzeni <lingedlamini@gmail.com>
2023-03-24 13:44:15 -0700

    Function Pointers Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,13 @@
+
+
 #include "3-calc.h"
 
 /**
- * op_add - calculates sum of integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: sum
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
  */
 int op_add(int a, int b)
 {
@@ -12,10 +15,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - calculates difference of integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: diference of a and b
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
@@ -23,34 +27,47 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiplies two integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: product of a and b
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
-	return (int a * int b);
+	return (a * b);
 }
 
 /**
- * op_div - calcultes division of integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: result of division
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
  */
 int op_div(int a, int b)
 {
-	return (int a / int b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
- * op_mod - calcultes remainder of division of integers
- * @a: 1st integer
- * @b: 2nd integer
- * Return: remainder of division
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
-	return (int a % int b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
